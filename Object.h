07154d0c4e7b04cfdc76134c9e4ec28c71d9af83
BigMet <34687892+BigMet@users.noreply.github.com>
2018-03-14 14:30:14 +0100

    Aggiornato parentesi a metodi virtual

--- Object.h
+++ Object.h
@@ -14,12 +14,12 @@ protected:
   public:
     Object(int _width, int _height);
 
-    virtual void onKeyLeft(CampoDiGioco *field);
-  	virtual void onKeyRight(CampoDiGioco *field);
-  	virtual void onKeyUp(CampoDiGioco *field);
-  	virtual void onKeyDown(CampoDiGioco *field);
-  	virtual void onKeyReleased();
-    virtual void draw(int x, int y, int _riga, int _colonna);
+    virtual void onKeyLeft(CampoDiGioco *field){}
+  	virtual void onKeyRight(CampoDiGioco *field){}
+  	virtual void onKeyUp(CampoDiGioco *field){}
+  	virtual void onKeyDown(CampoDiGioco *field){}
+  	virtual void onKeyReleased(){}
+    virtual void draw(int x, int y, int _riga, int _colonna){}
 
 
 };
